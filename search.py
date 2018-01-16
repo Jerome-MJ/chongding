4f91c805dc614d9a5af171334596f5f1570d27d0
liumengjie <jerome_android@163.com>
2018-01-16 08:23:49 +0800

    增加打开浏览器查找

--- search.py
+++ search.py
@@ -1,38 +1,39 @@
 #-*- coding: UTF-8 -*-
-import urllib.request
 from urllib.parse import quote
 
 import image_search
 import time
 import capture
 import requests
-
-
+import webbrowser
 def start(path):
     start=time.time()
     result_dic = image_search.getResult(path)
     question = result_dic.get("question")
     answer = result_dic.get("answer")
 
-    url = "http://m.baidu.com/s?wd=" + question
+    #url2 = "http://www.baidu.com/s?wd=" + question
+    url = "http://www.baidu.com/s?wd=" + question
     url = quote(url, safe="/:?=")
     response=requests.get(url)
     content=response.text
-    # result = urllib.request.urlopen(url)
-    # content = result.read()
-    # content = content.decode("utf-8")
     print(question)
-    size = len(answer)
-    for i in range(size):
-        print("%s\t%d" % (answer[i], content.count(answer[i])))
-    print(time.time()-start)
-
+    try:
+        size = len(answer)
+        for i in range(size):
+            ans=answer[i]
+            # if ans.isdigit():
+            #     print("%s\t%d" % (ans, content.count(ans)))
+            # else:
+            print("%s\t%d" % (ans, content.count(ans)))
+        print(time.time()-start)
+    except:
+        print("")
+    return url
 
 #截屏 获取地址
 path=capture.cap()
 
 # 开始识别
-start(path)
-
-
-
+url=start(path)
+webbrowser.open(url, new=0, autoraise=True)
